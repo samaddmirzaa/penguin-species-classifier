9e86dc3430887545d337a104b21ef9b80f366754
Mirza Samad <abdul.samad197@gmail.com>
2026-07-21 15:55:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,8 @@ target_mapper = {'Adelie': 0,
                  'Gentoo': 2}
 def target_encode(val):
   return target_mapper[val]
-
+y = y_raw.apply(target_encode)
+y
 
 
 with st.expander('Input Features'):
